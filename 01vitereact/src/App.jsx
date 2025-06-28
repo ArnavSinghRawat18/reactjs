f46fb04eb5ace91a14bfebc3cc90347f0deabc89
Arnav <rawatarnav180505@gmail.com>
2025-06-28 18:38:19 +0530

    first lecture

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from"./first"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/first.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h3>First program is ready</h3>
+    )
+}
+export default Chai;
